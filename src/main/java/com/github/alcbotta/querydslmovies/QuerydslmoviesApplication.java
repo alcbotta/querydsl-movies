32afed27f9c9029795c82a4788939ca215e3fd37
andre botta <alcbotta@gmail.com>
2021-07-16 19:30:31 +1000

    initial set up

---
+++ src/main/java/com/github/alcbotta/querydslmovies/QuerydslmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.github.alcbotta.querydslmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslmoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alcbotta/querydslmovies/QuerydslmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alcbotta.querydslmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
